d0633a9c0fd5973318a21dd99f311619a4c3ed48
Michael Fischl <michaelfischl1994@gmail.com>
2016-12-03 13:50:35 +0100

    Day 1 done

--- day_1/task_2.py
+++ day_1/task_2.py
@@ -4,6 +4,9 @@ with open('input', 'r') as f:
     position_x = 0
     position_y = 0
 
+    visited = []
+    visited.append((0, 0))
+
     c = 0
     while c < len(input):
         if (input[c] == 'R'):
@@ -18,8 +21,16 @@ with open('input', 'r') as f:
         while (c < len(input) and input[c] != 'L' and input[c] != 'R'):
             length += input[c]
             c += 1
-        if (orientation == 0): position_y += int(length)
-        if (orientation == 1): position_x += int(length)
-        if (orientation == 2): position_y -= int(length)
-        if (orientation == 3): position_x -= int(length)
+        to_walk = int(length)
+        already_visited = 0
+        for walk in range(to_walk):
+            if (orientation == 0): position_y += 1
+            if (orientation == 1): position_x += 1
+            if (orientation == 2): position_y -= 1
+            if (orientation == 3): position_x -= 1
+            if ((position_x, position_y) in visited):
+                already_visited = 1
+                break
+            visited.append((position_x, position_y))
+        if (already_visited == 1): break
     print "It is " + str(abs(abs(position_x) + abs(position_y))) + " blocks away!"
